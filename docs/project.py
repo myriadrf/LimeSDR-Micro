753a7c453cc43f8b1928e7d52fc9afb350197165
Andrew Back <andrew@abopen.com>
2026-04-23 20:30:23 +0100

    Docs: reconfigure for Cloudflare Workers.

--- docs/project.py
+++ docs/project.py
@@ -19,9 +19,9 @@ highlight_language = 'console'
 # Intersphinx mapping
 # To minimise build time only include projects that are referenced.
 intersphinx_internal = [
-    'quickstart',
-    'sdrgw',
-    'suiteng',
+#    'quickstart',
+#    'sdrgw',
+#    'suiteng',
 ]
 
 intersphinx_external = [
@@ -32,4 +32,4 @@ intersphinx_external = [
 archived = False
 
 # When True internal intersphinx targets point at stage.myriadrf.org.
-staging = True
+staging = False
